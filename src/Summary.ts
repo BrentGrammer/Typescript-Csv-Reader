550a9f3ed73e536e268ebe8f33e41be9a076bf6a
Brent M <brentonmarquez@gmail.com>
2019-07-01 19:57:52 -0600

    add static methods to eliminate verbose code

--- src/Summary.ts
+++ src/Summary.ts
@@ -1,4 +1,6 @@
 import { MatchData } from './MatchData';
+import { WinsAnalysis } from './analyzers/WinsAnalysis';
+import { HtmlReport } from './reportTargets/HtmlReport';
 
 /**
  * Classic Object Composition pattern:
@@ -15,6 +17,14 @@ export interface OutputTarget {
 }
 
 export class Summary {
+  /**  using a static method allows for loading a formatted report without having to create instances of Summary and pass in instances
+   * of the arguments.  It is a tool that can be used to make the code less verbose.
+   * You can return an instance of the same class with the other argument instances already passed in.
+   */
+
+  static winsAnalysisWithHtmlReport(team: string): Summary {
+    return new Summary(new WinsAnalysis(team), new HtmlReport());
+  }
   constructor(public analyzer: Analyzer, public outputTarget: OutputTarget) {}
 
   buildAndPrintReport(matches: MatchData[]): void {
--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,33 @@
 import { MatchReader } from './MatchReader';
-import { CsvFileReader } from './CsvFileReader';
-import { ConsoleReport } from './reportTargets/ConsoleReport';
-import { WinsAnalysis } from './analyzers/WinsAnalysis';
+// import { CsvFileReader } from './CsvFileReader';
+// import { ConsoleReport } from './reportTargets/ConsoleReport';
+// import { WinsAnalysis } from './analyzers/WinsAnalysis';
 import { Summary } from './Summary';
-import { HtmlReport } from './reportTargets/HtmlReport';
+// import { HtmlReport } from './reportTargets/HtmlReport';
 
 /**
  * Note: By convention, you do not export anything out of the index.ts file
  * You can move things to separate files and export them there.
+ *
+ * The commented out code is before the use of static methods which can be useful for making the code less verbose.
+ * The static methods return a new instance of the class they are defined in and also create instances of the argument objects
+ * referenced so that is all abstracted away by the static method.
  */
-const csvFileReader = new CsvFileReader('football.csv');
-const matchReader = new MatchReader(csvFileReader);
+// const csvFileReader = new CsvFileReader('football.csv');
+// const matchReader = new MatchReader(csvFileReader);
 
+// matchReader.load();
+
+const matchReader = MatchReader.fromCsv('football.csv');
 matchReader.load();
 
-const summary = new Summary(
-  new WinsAnalysis('Man United'),
-  //new ConsoleReport()
-  new HtmlReport()
-);
+// const summary = new Summary(
+//   new WinsAnalysis('Man United'),
+//   //new ConsoleReport()
+//   new HtmlReport()
+// );
 
-summary.buildAndPrintReport(matchReader.matches);
+// This example uses a static method to make the code less verbose so you don't have to create various instances
+Summary.winsAnalysisWithHtmlReport('Man United');
+
+//summary.buildAndPrintReport(matchReader.matches);
